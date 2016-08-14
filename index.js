2653883c57ace10098f08864bdcd7ef2db302bfe
Nickolay Ribal <elektronik2k5@gmail.com>
2016-08-14 18:34:51 +0300

    Add option to remove keys.

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 var yaml = require('js-yaml');
+var loaderUtils = require('loader-utils');
 
-module.exports = function (source) {
+function jsonValueReplacer(blacklist, key, value){
+  return blacklist.indexOf(key) > -1 ? undefined : value;
+}
+
+module.exports = function ymlLoader(source, map){
   var loader = this;
-
   loader.cacheable && loader.cacheable();
+
+  var query = loaderUtils.parseQuery(loader.query);
+  var keysToRemove = query.keysToRemove;
+
   var value = yaml.safeLoad(source, {
     filename: loader.resourcePath,
-    onWarning: function (error) {
+    onWarning: function emitLoaderWarning(error){
       loader.emitWarning(error.toString());
     }
   });
 
-  return 'module.exports = ' + JSON.stringify(value, null, '\t') + ';';
+  var anyKeysToReplace = Array.isArray(keysToRemove) && keysToRemove.length;
+  var replacer = anyKeysToReplace ? jsonValueReplacer.bind(keysToRemove) : undefined;
+
+  return [
+    'module.exports = ',
+    JSON.stringify(value, replacer , '\t'),
+    ';'
+  ].join('');
 };
