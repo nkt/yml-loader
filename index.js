77ca21662878006c56b525d91b82cbf3c9b47c10
Nickolay Ribal <elektronik2k5@gmail.com>
2016-08-14 19:05:20 +0300

    Add logging and tags.

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var yaml = require('js-yaml');
 var loaderUtils = require('loader-utils');
 
-function jsonValueReplacer(blacklist, key, value){
-  return blacklist.indexOf(key) > -1 ? undefined : value;
+function jsonValueReplacer(replacerConf, key, value){
+  var blacklist = replacerConf.blacklist || [];
+  var shouldRemoveKey = blacklist.indexOf(key) > -1;
+  if (shouldRemoveKey) {
+    console.info([
+      'Removing key ', key,
+      ' in file ', replacerConf.filename,
+      '.'
+    ].join('"'));
+  }
+  return shouldRemoveKey ? undefined : value;
 }
 
 module.exports = function ymlLoader(source, map){
@@ -11,20 +20,25 @@ module.exports = function ymlLoader(source, map){
 
   var query = loaderUtils.parseQuery(loader.query);
   var keysToRemove = query.keysToRemove;
+  var filename = loader.resourcePath;
 
-  var value = yaml.safeLoad(source, {
-    filename: loader.resourcePath,
+  var yamlFile = yaml.safeLoad(source, {
+    filename: filename,
     onWarning: function emitLoaderWarning(error){
       loader.emitWarning(error.toString());
     }
   });
 
   var anyKeysToReplace = Array.isArray(keysToRemove) && keysToRemove.length;
-  var replacer = anyKeysToReplace ? jsonValueReplacer.bind(keysToRemove) : undefined;
+  var replacerConf = {
+    filename: filename,
+    blacklist: keysToRemove
+  };
+  var replacerWithConf = anyKeysToReplace ? jsonValueReplacer.bind(replacerConf) : undefined;
 
   return [
     'module.exports = ',
-    JSON.stringify(value, replacer , '\t'),
+    JSON.stringify(yamlFile, replacerWithConf, '\t'),
     ';'
   ].join('');
 };
